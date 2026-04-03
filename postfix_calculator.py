62dff9e7fc5fee5c03a243a5bb25b136a20491bf
Jordan <133260935+JYemer@users.noreply.github.com>
2026-04-03 16:05:11 -0600

    Implement postfix evaluation logic and variable handling

--- postfix_calculator.py
+++ postfix_calculator.py
@@ -24,14 +24,14 @@ class PostfixCalculator:
             key (str): Variable name
             value (int): Variable value
         """
-        # TODO: Insert variable into BST
+        #Insert variable and its value into BST
         self.variable_tree.insert(key, value)
         pass
 
     def delete_all_variables(self):
         """Delete all variables from the BST."""
-        # TODO: Clear the BST
-        self.variable_tree.clear()
+        #Resets calculator's memory by clearing tree
+        self.variable_tree.delete_all()
         pass
 
     def evaluate_postfix_expression(self, expression):
@@ -44,22 +44,48 @@ class PostfixCalculator:
         Returns:
             int: Final evaluated result
         """
-        # TODO:
-        # 1. Split expression into tokens
-        # 2. Process each token
-        # 3. Push integers or variable values onto stack
-        # 4. Apply operators
-        # 5. Return final result
+
+        #Converts the string into a list of strings
+        tokens = expression.split()
+        #Iterate through each token in the list
+        for token in tokens:
+            #If the token is a direct number, convert string to int and push it onto the stack
+            if self.is_integer(token):
+                self.stack.push(int(token))
+            #If the token is an operator, pop the two items; apply the operator and push the result onto the stack
+            elif self.is_operator(token):
+                # Pop operands in reverse order (right then left)
+                right = self.stack.pop()
+                left = self.stack.pop()
+                #Perform calculation and push the result onto the stack
+                result = self.apply_operator(token, left, right)
+                self.stack.push(result)
+            else:
+                # Assume it's a variable; look it up in the BST
+                value = self.variable_tree.search(token)
+                #Push the found value onto the stack
+                self.stack.push(value)
+
+        # The final result should be the only thing left on the stack
+        return self.stack.pop()
+    
         pass
 
     def is_operator(self, token):
         """Return True if token is a valid operator."""
-        # TODO: Check for +, -, *, /
+        #Checks if the string matches one of the four operators
+        return token in ('+', '-', '*', '/')
         pass
 
     def is_integer(self, token):
         """Return True if token can be converted to an integer."""
-        # TODO: Try converting token to int
+        #Return true if the token can be converted to an int
+        try:
+            int(token)
+            return True
+        #Return false if it can't be converted to an int
+        except ValueError:
+            return False
         pass
 
     def apply_operator(self, operator, left, right):
@@ -74,5 +100,16 @@ class PostfixCalculator:
         Returns:
             int: Result of operation
         """
-        # TODO: Perform the correct arithmetic operation
+        #Perform arithmetic operation based on operator
+        if operator == '+':
+            return left + right
+        elif operator == '-':
+            return left - right
+        elif operator == '*':
+            return left * right
+        elif operator == '/':
+            if right == 0:
+                raise ZeroDivisionError("Division by zero")
+            return left // right # Using integer division for consistency
+        return 0
         pass
